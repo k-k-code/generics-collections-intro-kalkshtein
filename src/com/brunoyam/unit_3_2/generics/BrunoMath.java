b6ee59db221fa70621df9b3dec72bd6e39601177
kalkshtein <kalkshtein@mail.ru>
2019-11-28 19:07:58 +0300

    фваф

--- src/com/brunoyam/unit_3_2/generics/BrunoMath.java
+++ src/com/brunoyam/unit_3_2/generics/BrunoMath.java
@@ -1,37 +1,29 @@
 package com.brunoyam.unit_3_2.generics;
 
-public final class BrunoMath<T> {
+public final class BrunoMath<T extends Number> {
 
     private BrunoMath() {}
 
 
     public BrunoMath<T> sum(T[] values) {
-        BrunoMath<T> result = new BrunoMath<>();
-        for(T value : values) {
-            result = (BrunoMath<T>) value;
-        }
-        return result;
+        return this.sum(values);
     }
 
-    private static int max(int a, int b) {
-        return a > b ? a : b;
+    public BrunoMath<T> max(int a, int b) {
+        return this.max(a, b);
     }
 
-    public static int max(int[] values) {
-        if (values.length == 0) return 0;
-
-        int result = values[0];
-        for (int i = 1; i < values.length; i++) {
-            result = max(result, values[i]);
-        }
-        return result;
-    }
+//    public static int max(int[] values) {
+//        if (values.length == 0) return 0;
+//
+//        return this.max();
+//    }
 
     public BrunoMath<T> avg(T[] values) {
-        if (values.length == 0) return null;
-
-        BrunoMath<T> sum = sum(values);
-        return sum / values.length;
-    }
+//        if (values.length == 0) return null;
+//
+//        BrunoMath<T> sum = sum(values);
+        return this.avg(values);
+  }
 
 }
